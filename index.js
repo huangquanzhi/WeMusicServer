7bcd09e0b7a78403347124de27bcfb16e800c96c
Jackie <quanzhi.huang@scotiabank.com>
2016-10-13 16:16:10 -0400

    WIP: Doesnt work

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ var path = require('path');
 var formidable = require('formidable');
 var fs = require('fs');
 
-app.post('/upload', function(req, res){
+app.get('/', function(req,res){
+   res.send("GFettt");
+});
 
+app.post('/upload', function(req, res){
+    res.send("Incomeing request")
     // create an incoming form object
     var form = new formidable.IncomingForm();
 
@@ -18,6 +22,7 @@ app.post('/upload', function(req, res){
     // every time a file has been uploaded successfully,
     // rename it to it's orignal name
     form.on('file', function(field, file) {
+        console.log("File added");
         fs.rename(file.path, path.join(form.uploadDir, file.name));
     });
 
@@ -29,7 +34,7 @@ app.post('/upload', function(req, res){
     // once all the files have been uploaded, send a response to the client
     form.on('end', function() {
         console.log("Done")
-        res.end('success');
+        res.end(200);
     });
 
     // parse the incoming request containing the form data
@@ -37,6 +42,6 @@ app.post('/upload', function(req, res){
 
 });
 
-var server = app.listen(8081, function(){
-    console.log('Server listening on port 8081');
+var server = app.listen(6666, function(){
+    console.log('Server listening on port 6666');
 });
